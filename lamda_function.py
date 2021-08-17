9d489eb584246fa1739b4fc800b6c7337b7987e8
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-08-17 14:28:10 +0530

    Add files via upload

---
+++ lamda_function.py
@@ -0,0 +1,5 @@
+a=int(input("enter the values of a\n"))
+b=int(input("enter the values of b\n"))
+k=lambda a,b:a+b#lambda function is one line function and it takes twoo values of a and b  and acts according to  the conditiion in this context
+print(f"the sum of the two numbers is {k(a,b)}")
+
