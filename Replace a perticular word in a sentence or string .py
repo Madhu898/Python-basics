1a458e13a3a301263fc7b86ece4acdaf5c32ddcb
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-12-10 15:36:17 +0530

    Create Replace a perticular word in a sentence or string .py

---
+++ Replace a perticular word in a sentence or string .py
@@ -0,0 +1,17 @@
+'''
+ input 1 = 
+            there freedom       #input string
+            there               #word which has to be replaced
+            thier               #the new replacing word
+ output 1 =
+            thier freedom  
+
+'''
+
+
+
+s1=input()
+s2=input()
+s3=input()
+s1=s1.replace(s2,s3)
+print(s1)
