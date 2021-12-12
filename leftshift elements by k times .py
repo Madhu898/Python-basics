947c537b5ca2393ae39a9b19dd084c7e9b3ea471
Madhusudhan <79078323+Madhu898@users.noreply.github.com>
2021-12-12 13:24:03 +0530

    Create leftshift elements by k times .py

---
+++ leftshift elements by k times .py
@@ -0,0 +1,29 @@
+
+'''
+                                                input 1  = 
+                                                              5                  =  no of elements in list
+                                                              10 20 30 40 50     =  elements of list
+                                                              2				           =  k times shift
+
+
+                                                output 1 =
+                                                              30 40 50 10 20	   =  after k times shift
+'''
+
+
+
+a=[]
+n=int(input())
+for i in range(n):
+	ele=int(input())
+	a.append(ele)
+k=int(input())
+for i in range(k):
+	temp=a[0]
+	for j in range(len(a)-1):
+		a[j]=a[j+1]
+
+	a[len(a)-1]=temp
+for i in a:
+	print(i,end=" ")		
+
